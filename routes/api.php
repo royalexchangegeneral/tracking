2206880799097bb9c991bf75a5b6989a4daa0b5c
ukajose <75024044+royalexchangegeneral@users.noreply.github.com>
2021-08-04 19:58:50 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/newregister',[CustomerController::class,'newregister'])->name('newregister');
+Route::post('/attendance',[CustomerController::class,'attendance'])->name('attendance');
